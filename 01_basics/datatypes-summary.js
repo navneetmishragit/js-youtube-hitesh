dfb2cf89ed569c22e76b1515b0fa13a1050c3df3
navneetmishragit <128609424+navneetmishragit@users.noreply.github.com>
2023-10-13 15:52:47 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+//Primitivve(call by value)
+
+//7 type:: String,Number,Boolean,null.undefined,symbol,BigInt
+
+//JS is dynamically typed language
+
+//Reference(Non Primitive)
+
+//Array, Objects, Functions
+
+const heroes = ["shaktimaan","hero"]
+
+const myObj={
+    name:"navneet",
+    age:25,
+    occuptaion: "SDE"
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
