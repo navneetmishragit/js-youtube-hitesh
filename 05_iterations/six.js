9e56a1801be1c339d7dd3ad7569c1d5f5b622a35
navneetmishragit <128609424+navneetmishragit@users.noreply.github.com>
2023-10-16 18:48:29 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,17 @@
+const myNum=[1,2,3,4,5,6,7,8,9,10]
+
+// const newNum=myNum.filter((num)=>num>5)
+
+// console.log(newNum);
+
+
+// const newNum=[]
+
+// myNum.forEach((num)=>{
+//     if(num>5){
+//         newNum.push(num)
+//     }
+// })
+
+// console.log(newNum);
+
